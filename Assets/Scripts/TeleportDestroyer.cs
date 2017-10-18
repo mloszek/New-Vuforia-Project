a3370a49ec865232ef2a738c05ae1a0bbdb89eae
milosz <miloszloszek@gmail.com>
2017-10-18 18:35:29 +0200

    next_commit

---
+++ Assets/Scripts/TeleportDestroyer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportDestroyer : MonoBehaviour {
+
+
+	void Start () {
+		StartCoroutine (kill ());
+	}
+
+	IEnumerator kill ()
+	{
+		yield return new WaitForSeconds (1.5f);
+
+		Destroy (gameObject);
+	}
+}
